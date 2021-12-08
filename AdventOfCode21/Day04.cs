4afe557666992c6c78264503a0b0138c0b8981ad
woltob <Tobias.Wolf@orbis.de>
2021-12-08 14:07:49 +0100

    Day4 solved

--- AdventOfCode21/Day04.cs
+++ AdventOfCode21/Day04.cs
@@ -10,14 +10,54 @@ namespace AdventOfCode21
 {
     public class Day04 : BaseDay
     {
-        private readonly List<int[,]> Boards = new List<int[,]>();
-        private readonly int[] WinningNumbers;
+        private readonly List<int[,]> _Boards = new List<int[,]>();
+        private readonly int[] _WinningNumbers;
+
+        internal class BingoBoard
+        {
+            public int[,] Board;
+            public SortedList<int, int> RowMarks;
+            public SortedList<int, int> ColumnMarks;
+            public int WinRank;
+            public int LastWinNumber;
+            public bool HasWon = false;
+
+
+            private static int RankCounter = 1;
+
+            public BingoBoard(int[,] Board)
+            {
+                this.Board = Board;
+                RowMarks = new SortedList<int, int>() { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 } };
+                ColumnMarks = new SortedList<int, int>() { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 } };
+
+            }
+            public bool MarkHit(int rowIndex, int columnIndex, int winNumber)
+            {
+                this.Board[rowIndex, columnIndex] = -1;
+                this.RowMarks[rowIndex]++;
+                this.ColumnMarks[columnIndex]++;
+                if (!this.HasWon && (RowMarks.Values.Contains(5) || ColumnMarks.Values.Contains(5)))
+                {
+                    this.LastWinNumber = winNumber;
+                    this.WinRank = RankCounter++;
+                    this.HasWon = true;
+                    return true;
+                }
+               
+                return false;
+            }
+            public int Sum()
+            {
+                return this.Board.Cast<int>().Where(x => x > -1).ToArray().Sum();
+            }
+        }
 
 
         public Day04()
         {
             var all = File.ReadAllLines(InputFilePath).ToArray();
-            WinningNumbers = all.First().Split(",").Select(x => Convert.ToInt32(x)).ToArray();
+            _WinningNumbers = all.First().Split(",").Select(x => Convert.ToInt32(x)).ToArray();
             all = all.Skip(2).ToArray();
             all = all.Where(line => line != String.Empty).ToArray();
             if (all.Length % 5 != 0)
@@ -33,14 +73,14 @@ namespace AdventOfCode21
                         newBoard[j, k] = lineNumbers[k];
                     }
                 }
-                Boards.Add(newBoard);
+                _Boards.Add(newBoard);
             }
         }
         public override ValueTask<string> Solve_1()
         {
 
-            var bingoBoards = this.Boards.Select(x => new BingoBoard(x)).ToList();
-            foreach (var number in WinningNumbers)
+            var bingoBoards = _Boards.Select(x => new BingoBoard((int[,])x.Clone())).ToList();
+            foreach (var number in _WinningNumbers)
             {
                 foreach (var bingoBoard in bingoBoards)
                 {
@@ -50,10 +90,8 @@ namespace AdventOfCode21
                         {
                             if (bingoBoard.Board[i, j] == number)
                             {
-                                if (bingoBoard.MarkHit(i, j))
-                                {
+                                if (bingoBoard.MarkHit(i, j,number))
                                     return new((bingoBoard.Sum() * number).ToString());
-                                }
                             }
                         }
                     }
@@ -62,41 +100,30 @@ namespace AdventOfCode21
             return new("");
         }
 
-        internal class BingoBoard
-        {
-            public int[,] Board;
-            public SortedList<int, int> RowMarks;
-            public SortedList<int, int> ColumnMarks;
 
-            public BingoBoard(int[,] Board)
-            {
-                this.Board = Board;
-                RowMarks = new SortedList<int, int>() { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 } };
-                ColumnMarks = new SortedList<int, int>() { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 } };
-            }
-            public bool MarkHit(int rowIndex, int columnIndex)
-            {
-                this.Board[rowIndex, columnIndex] = -1;
-                this.RowMarks[rowIndex]++;
-                this.ColumnMarks[columnIndex]++;
-                if (RowMarks.Values.Contains(5) || ColumnMarks.Values.Contains(5))
-                    return true;
-                return false;
-            }
-            public int Sum()
-            {
-                return this.Board.Cast<int>().Where(x => x > -1).ToArray().Sum();
-            }
-        }
 
         public override ValueTask<string> Solve_2()
         {
-            //throw new NotImplementedException();
-            return new("");
+            var bingoBoards = _Boards.Select(x => new BingoBoard((int[,])x.Clone())).ToList();
+            foreach (var number in _WinningNumbers)
+            {
+                foreach (var bingoBoard in bingoBoards)
+                {
+                    if (bingoBoard.HasWon)
+                        continue;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (bingoBoard.Board[i, j] == number)
+                                bingoBoard.MarkHit(i, j, number);
+                        }
+                    }
+                }
+            }
+            bingoBoards = bingoBoards.OrderBy(e => e.WinRank).ToList();
+            return new((bingoBoards.Last().Sum() * bingoBoards.Last().LastWinNumber).ToString());
         }
-
-
-
 
     }
 }
